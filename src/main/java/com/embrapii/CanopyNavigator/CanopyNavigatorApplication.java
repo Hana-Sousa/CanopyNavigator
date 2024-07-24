00cc39b6a8e01d05b6ca31ec75b176e0fe982bd8
Hana Gabriela Albuquerque Sousa <hanags@JX6FKY2VNQ.tld>
2024-07-24 11:54:39 -0300

    first commit

---
+++ src/main/java/com/embrapii/CanopyNavigator/CanopyNavigatorApplication.java
@@ -0,0 +1,13 @@
+package com.embrapii.CanopyNavigator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanopyNavigatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanopyNavigatorApplication.class, args);
+	}
+
+}
